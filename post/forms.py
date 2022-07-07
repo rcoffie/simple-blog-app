54def10fdf981a703c52025669178889ae8f270f
Robert Coffie <rcoffie22@yahoo.com>
2022-07-07 19:14:38 +0000

    adding post CRUD

---
+++ post/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from post.models import Posts
+
+class PostsForm(ModelForm):
+    class Meta:
+        model = Posts
+        fields = ['title','author','content','status','category']
---
+++ post/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render,redirect
+from post. forms import PostsForm
+from post.models import Posts
+# Create your views here.
+
+
+def posts_list(request):
+    posts = Posts.objects.all()
+    context = {'posts':posts}
+    return render(request,'posts/post_list.html',context)
+
+
+def create_post(request):
+    create_post_form = PostsForm()
+    if request.method == 'POST':
+        create_post_form = PostsForm(request.POST)
+        if create_post_form.is_valid():
+            create_post_form.save()
+            return redirect('posts_list')
+    context = {'create_post_form':create_post_form}
+    return render(request, 'posts/create_post.html',context )
+
+
+def post_detail(request, id):
+    post = Posts.objects.get(id=id)
+    context = {'post':post}
+    return render(request,'posts/post_detail.html',context)
+
+def update_post(request, id):
+    post = Posts.objects.get(id=id)
+    update_post_form = PostsForm(instance=post)
+    if request.method == 'POST':
+        update_post_form = PostsForm(request.POST, instance=post)
+        if update_post_form.is_valid():
+            update_post_form.save()
+            return redirect('posts_list')
+    context = {'update_post_form':update_post_form,}
+    return render(request, 'posts/update_post.html',context)
+
+
+def delete_post(request, id):
+    post = Posts.objects.get(id=id)
+    post.delete()
+    return redirect('posts_list')
